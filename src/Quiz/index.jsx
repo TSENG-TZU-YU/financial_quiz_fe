328611584c651f543c8489de0441f5091fbfa46d
TSENG-TZU-YU <46403343+TSENG-TZU-YU@users.noreply.github.com>
2023-03-21 16:18:31 +0800

    text anim  one page

--- src/Quiz/index.jsx
+++ src/Quiz/index.jsx
@@ -1,19 +1,57 @@
-import { React, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 
 import './index.scss';
 
-function QuizOne() {
+function Quiz() {
+    const [start, setStart] = useState(false);
     const [quit1Val, setQuit1Val] = useState('');
     const [quit2Val, setQuit2Val] = useState('');
 
     console.log('object', quit1Val);
+    useEffect(() => {
+        const hero = document.querySelector('.main');
+        const text = hero.querySelector('.h1');
+        function shadow(e) {
+            const width = hero.offsetWidth;
+            const height = hero.offsetHeight;
+            const walk = 50;
+
+            let { offsetX: x, offsetY: y } = e;
+
+            if (this !== e.target) {
+                x = x + e.target.offsetLeft;
+                y = y + e.target.offsetTop;
+            }
+
+            //how far should the text-shadow go
+            const xWalk = Math.round((x / width) * walk - walk / 2);
+            const yWalk = Math.round((y / height) * walk - walk / 2);
+
+            text.style.textShadow = `
+          ${xWalk}px ${yWalk}px 0 #33cccc
+          `;
+        }
+
+        hero.addEventListener('mousemove', shadow);
+    }, []);
     return (
         <>
             <div className="oneContainer">
-            {/* <div className="start">
-                <button>start</button>
-            </div> */}
-                <div className="one">
+                <div className="start">
+                    <div className="main">
+                        <div className="h1">Follow me around.</div>
+                        <div className="h2">Follow me around.</div>
+                        <button
+                            className="startBtn btn"
+                            onClick={() => {
+                                setStart(true);
+                            }}
+                        >
+                            START
+                        </button>
+                    </div>
+                </div>
+                <div className={`one  ${start ? 'scrollPage' : ''}`}>
                     <div>
                         <input
                             type="radio"
@@ -121,4 +159,4 @@ function QuizOne() {
     );
 }
 
-export default QuizOne;
+export default Quiz;
--- src/QuizTwo/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function QuizTwo() {
-  return (
-    <div>QuizOne</div>
-  )
-}
-
-export default QuizTwo
