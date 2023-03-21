a7345d4ab37042a991d820d3767a0b7a6bec5c00
TSENG-TZU-YU <46403343+TSENG-TZU-YU@users.noreply.github.com>
2023-03-21 09:50:10 +0800

    create route

---
+++ src/QuizOne/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function QuizOne() {
+  return (
+    <div>QuizOne</div>
+  )
+}
+
+export default QuizOne
---
+++ src/QuizTwo/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function QuizTwo() {
+  return (
+    <div>QuizOne</div>
+  )
+}
+
+export default QuizTwo
